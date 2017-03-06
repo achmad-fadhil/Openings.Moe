b3e4bb56ff62a2459522b15841f4bff86f1923a2
Achmad Fadhil <fadhil.aprilianto@gmail.com>
2017-03-06 12:00:33 +0700

    added sort & filter

--- Openings.Moe.Core/ViewModels/OpeningListViewModel.cs
+++ Openings.Moe.Core/ViewModels/OpeningListViewModel.cs
@@ -37,9 +37,26 @@ namespace Openings.Moe.Core.ViewModels
             set
             {
                 _searchQuery = value;
-                Filter(value);
+                Search(value);
                 RaisePropertyChanged(() => SearchQuery);
             }
+        }
+
+        private bool _sortMode = false;
+        public bool SortMode
+        {
+            get { return _sortMode; }
+            set { _sortMode = value; RaisePropertyChanged(() => SortMode); }
+        }
+
+        private List<string> _sortType = new List<string> { "Source", "Song Title", "Song Artist" };
+        private List<string> _filterType = new List<string> { "Opening", "Ending", "All" };
+
+        private List<string> _options;
+        public List<string> Options
+        {
+            get { return _options; }
+            set { _options = value; RaisePropertyChanged(() => Options); }
         }
 
         public OpeningListViewModel(IOpeningService openingService, IDialogService dialogService)
@@ -74,7 +91,7 @@ namespace Openings.Moe.Core.ViewModels
             }
         }
 
-        private void Filter(string query)
+        private void Search(string query)
         {
             if (string.IsNullOrEmpty(query))
             {
@@ -82,10 +99,85 @@ namespace Openings.Moe.Core.ViewModels
             }
             else
             {
-                FilteredOpenings = Openings.Where(o => o.Source.ToLower().Contains(query)).ToList();
+                query = query.ToLower();
+                FilteredOpenings = Openings
+                    .Where(o => 
+                        o.Source.ToLower().Contains(query)
+                        || (o.Song != null 
+                            && (o.Song.Artist.ToLower().Contains(query) 
+                                || o.Song.Title.ToLower().Contains(query)))
+                    )
+                    .ToList();
             }
         }
 
+        private bool isSort = false;
+        private bool isFilter = false;
+
+        public IMvxCommand OpenSortCommand => new MvxCommand(DoOpenSort);
+
+        private void DoOpenSort()
+        {
+            Options = _sortType;
+            isSort = !isSort;
+            isFilter = false;
+            SortMode = isSort;
+        }
+
+        public IMvxCommand OpenFilterCommand => new MvxCommand(DoOpenFilter);
+
+        private void DoOpenFilter()
+        {
+            Options = _filterType;
+            isFilter = !isFilter;
+            isSort = false;
+            SortMode = isFilter;
+        }
+
+        public IMvxCommand SortCommand => new MvxCommand<string>(DoSort);
+
+        private void DoSort(string param)
+        {
+            if (isFilter)
+            {
+                switch (param)
+                {
+                    case "All":
+                        FilteredOpenings = Openings;
+                        break;
+                    default:
+                        FilteredOpenings = Openings.Where(o => o.Title.ToLower().Contains(param.ToLower())).ToList();
+                        break;
+                }
+                return;
+            }
+
+            switch (param)
+            {
+                case "Source":
+                    FilteredOpenings = FilteredOpenings.OrderBy(o => o.Source).ToList();
+                    break;
+                case "Song Artist":
+                    FilteredOpenings = FilteredOpenings
+                        .Where(o => o.Song != null)
+                        .OrderBy(o => o.Song.Artist)
+                        .Concat(FilteredOpenings
+                            .Where(x => x.Song == null)
+                            .OrderBy(x => x.Source))
+                        .ToList();
+                    break;
+                case "Song Title":
+                    FilteredOpenings = FilteredOpenings
+                        .Where(o => o.Song != null)
+                        .OrderBy(o => o.Song.Title)
+                        .Concat(FilteredOpenings
+                            .Where(x => x.Song == null)
+                            .OrderBy(x => x.Source))
+                        .ToList();
+                    break;
+            }
+        }
+        
         public IMvxCommand OpenDetailCommand => new MvxCommand<Opening>(DoOpenDetail);
 
         private void DoOpenDetail(Opening param)
